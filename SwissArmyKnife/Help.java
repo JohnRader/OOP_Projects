7e014448c3df8b6dacfb85511d3457c7a5621db5
John Rader <jcrader238@gmail.com>
2018-04-23 12:16:21 -0500

    Added more projects

---
+++ SwissArmyKnife/Help.java
@@ -0,0 +1,8 @@
+class Help {
+    public static void printHelp() {
+        System.out.println("Welcome to the Swiss Army Knife Application!");
+        System.out.println("Usage: sak [-HttpRequest <url>] [-HttpRequestFile <FILENAME.json>]"
+        +" [-HttpRequestFileMultiThreaded <FILENAME.json>]");
+        System.out.println("More commands to be added soon...");
+    }
+}
