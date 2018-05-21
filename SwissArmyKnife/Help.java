bcd5c06a4988ea113c80efdae0610e61e09eabb2
John Rader <32828579+JohnRader@users.noreply.github.com>
2018-05-21 11:00:13 -0500

    Delete Help.java

--- SwissArmyKnife/Help.java
+++
@@ -1,8 +0,0 @@
-class Help {
-    public static void printHelp() {
-        System.out.println("Welcome to the Swiss Army Knife Application!");
-        System.out.println("Usage: sak [-HttpRequest <url>] [-HttpRequestFile <FILENAME.json>]"
-        +" [-HttpRequestFileMultiThreaded <FILENAME.json>]");
-        System.out.println("More commands to be added soon...");
-    }
-}
